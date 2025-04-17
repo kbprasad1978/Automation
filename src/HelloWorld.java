20793a993d5e47311d2d507206f57d93adfc4dab
kbprasad1978@gmail.com <Marusheka@4315>
2025-04-17 11:10:04 +0100

    This is my first commit on the feature branch/RMS-123

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ class HelloWorld {
         System.out.println("This is commit from feature into master");
         System.out.println("This demo for no conflict");
         System.out.println("This demo");
+        System.out.println("This is demo1");
     }
 
     void hello(){
