412e8efd220988a4d9da06a57581cd4596294f52
kbprasad1978 <kbprasad1978@gmail.com>
2025-04-17 16:32:40 +0100

    Demo for git rebase
    This is testing rebase and merge functionality

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld {
         System.out.println("This demo for no conflict");
         System.out.println("This demo");
         System.out.println("This is demo1");
+        System.out.println("This is demo2");
     }
 
     void hello(){
